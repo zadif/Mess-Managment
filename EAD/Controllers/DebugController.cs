d089a24efeb8fbc07ea44c06b0366f308bd69105
Zadif <zadifmustafa93@gmail.com>
2026-01-01 21:28:32 +0500

    Fixing things for deployment

---
+++ EAD/Controllers/DebugController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EAD.Models;
+
+namespace EAD.Controllers
+{
+    public class DebugController : Controller
+    {
+        private readonly EadProjectContext _context;
+
+        public DebugController(EadProjectContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/migrate-db")]
+        public IActionResult Migrate()
+        {
+            try
+            {
+                _context.Database.Migrate();  // This creates all tables
+                return Content("<h2>Success!</h2> Database tables created successfully.<br><br>Tables: Users, MealItems, Bills, DailyConsumptions, etc.<br><br>You can now delete this controller.");
+            }
+            catch (Exception ex)
+            {
+                return Content("<pre>Error: " + ex.Message + "\n\n" + ex.InnerException?.Message + "</pre>");
+            }
+        }
+    }
+}
